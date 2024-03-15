fa861ccdceaea2da4ba4f1df08c91d90bae29593
chagsn <102815082+chagsn@users.noreply.github.com>
2024-03-15 14:45:52 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
     key    = "states/terraform.tfstate"
     region = "us-east-1"
     access_key = "${AWS_ACCESS_KEY_ID}"
-    secret_key = "${AWS_SECRET_ACCESS_KEY}
+    secret_key = "${AWS_SECRET_ACCESS_KEY}"
   }
 }
  
